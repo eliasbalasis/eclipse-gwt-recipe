18a6cc2e834b2954bd60414f3cf45653e410b200
eliasbalasis <eliasbalasis@gmail.com>
2021-01-16 18:49:38 +0000

    GWT structure and dependency exclusions

---
+++ modules/gwt-web-client/src/main/java/eliasbalasis/gwt/client/Test.java
@@ -0,0 +1,10 @@
+package eliasbalasis.gwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Test implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+}
---
+++ modules/gwt-web-shared/src/main/java/eliasbalasis/gwt/shared/SharedClass.java
@@ -0,0 +1,9 @@
+package eliasbalasis.gwt.shared;
+
+public final class SharedClass {
+
+	public final String CLIENT_MESSAGE = "This is client-side text";
+
+	private SharedClass() {
+	}
+}
